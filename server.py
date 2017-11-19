9de914a2b6970080cc6d274627c2029cc6d2c127
Daniel Widmann <widmann.daniel@gmail.com>
2017-11-19 22:12:26 +0100

    feat(server): use reliable provider

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_restful import Api
 
 from credential_store import CredentialStore
+from geocode_provider_google import GeocodeProviderGoogle
 from geocode_provider_here import GeocodeProviderHere
+from geocode_provider_reliable import GeocodeProviderReliable
 from geocode_resource import GeocodeResource
 
 
@@ -11,7 +13,9 @@ def run_server(host, port, debug):
     api = Api(app)
 
     # setup the provider we are going to use for resolving requests
-    geocode_provider = GeocodeProviderHere(CredentialStore())
+    geocode_provider = GeocodeProviderReliable(
+        GeocodeProviderHere(CredentialStore()),
+        GeocodeProviderGoogle(CredentialStore()))
 
     # create and register the resource
     api.add_resource(GeocodeResource, '/geocode', resource_class_kwargs={'geocode_provider': geocode_provider})
